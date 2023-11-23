3284ca41782513ad647776cf07d88a36ea2abc7e
carloscamacho <carlosalvarez9805@gmail.com>
2023-11-23 17:33:19 -0600

    feat: add x-data-pickers dependencies

--- src/EventManagementApp.jsx
+++ src/EventManagementApp.jsx
@@ -1,10 +1,16 @@
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+
 import { AppRouter } from './router/AppRouter';
 import { AppTheme } from './themes';
 
+
 export const EventManagementApp = () => {
     return (
-        <AppTheme>
-            <AppRouter />
-        </AppTheme>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <AppTheme>
+                <AppRouter />
+            </AppTheme>
+        </LocalizationProvider>
     )
 }
