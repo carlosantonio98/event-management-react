bc9a25ea098072f3516dc78302dd74ef530ffeb3
carloscamacho <carlosalvarez9805@gmail.com>
2023-11-24 19:46:36 -0600

    feat: show events in table

--- src/event/pages/EventPage.jsx
+++ src/event/pages/EventPage.jsx
@@ -1,6 +1,7 @@
-import { Button, IconButton, Stack, TableHead, Typography } from '@mui/material';
-import { EventLayout } from '../layout/EventLayout';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
+import { Button, CircularProgress, IconButton, Stack, TableHead, Typography } from '@mui/material';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -9,60 +10,40 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Delete, Edit } from '@mui/icons-material';
 
-import { Link } from 'react-router-dom';
-  
-const rows = [
-    {
-        "id": 1,
-        "name": "jose",
-        "description": "jose test",
-        "startDate": null,
-        "endDate": null,
-        "categoryFk": 1
-    },
-    {
-        "id": 2,
-        "name": "jose",
-        "description": "jose test",
-        "startDate": null,
-        "endDate": null,
-        "categoryFk": 1
-    },
-    {
-        "id": 3,
-        "name": "jose",
-        "description": "jose test",
-        "startDate": null,
-        "endDate": null,
-        "categoryFk": 1
-    },
-    {
-        "id": 4,
-        "name": "jose test 8",
-        "description": "jose test 28",
-        "startDate": null,
-        "endDate": null,
-        "categoryFk": 1
-    },
-    {
-        "id": 5,
-        "name": "jose test 8",
-        "description": "jose test 28",
-        "startDate": null,
-        "endDate": null,
-        "categoryFk": 1
-    },
-    {
-        "id": 6,
-        "name": "jose test 82",
-        "description": "jose test 28",
-        "startDate": null,
-        "endDate": null,
-        "categoryFk": 1
-    }
-];
+import axiosClient from '../../axios-client';
+
+import { EventLayout } from '../layout/EventLayout';
 
 export const EventPage = () => {
+    const [ events, setEvents ] = useState([]);
+    const [ isLoading, setIsLoading ] = useState(false);
+
+    const getEvents = () => {
+        setIsLoading(true);
+
+        axiosClient.get("/events")
+            .then(({ data }) => {
+                setEvents( data );
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                if (axiosClient.isCancel(error)) {
+                    console.log('Solicitud cancelada:', error.message);
+                } else {
+                    console.error('Error fetching data:', error);
+                }
+
+                setIsLoading(false);
+            });
+    }
+
+    useEffect(() => {
+        getEvents();
+
+        return(() => {
+            getEvents();
+        })
+    }, []);
 
     const onDelete = (eventId) => {
         if(!window.confirm('Are you sure you want to delete this user?')) {
@@ -96,22 +77,30 @@ export const EventPage = () => {
                     </TableRow>
                     </TableHead>
                     <TableBody>
-                        {rows.map((row) => (
-                            <TableRow
-                                key={row.id}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="row">{row.name}</TableCell>
-                                <TableCell align="right">{row.description}</TableCell>
-                                <TableCell align="right">{row.startDate ?? "Sin asignar"}</TableCell>
-                                <TableCell align="right">{row.endDate ?? "Sin asignar"}</TableCell>
-                                <TableCell align="right">{row.categoryFk}</TableCell>
-                                <TableCell align="right">
-                                    <IconButton component={Link} to={"/events/" + row.id} ><Edit/></IconButton>
-                                    <IconButton onClick={ () => onDelete(row.id) }><Delete/></IconButton>
-                                </TableCell>
-                            </TableRow>
-                        ))}
+                        {
+                            isLoading && 
+                            <CircularProgress />
+                        }
+
+                        {
+                            !isLoading && events.map((row) => (
+                                <TableRow
+                                    key={row.id}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+
+                                    <TableCell component="th" scope="row">{row.name}</TableCell>
+                                    <TableCell align="right">{row.description}</TableCell>
+                                    <TableCell align="right">{row.startDate ?? "Sin asignar"}</TableCell>
+                                    <TableCell align="right">{row.endDate ?? "Sin asignar"}</TableCell>
+                                    <TableCell align="right">{row.categoryFk}</TableCell>
+                                    <TableCell align="right">
+                                        <IconButton component={Link} to={"/events/" + row.id} ><Edit/></IconButton>
+                                        <IconButton onClick={ () => onDelete(row.id) }><Delete/></IconButton>
+                                    </TableCell>
+
+                                </TableRow>
+                            ))
+                        }
                     </TableBody>
                 </Table>
             </TableContainer>
